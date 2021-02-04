064f6c9cc79981922706df1dab2288a6f00aa745
elmghraaby <ahmed.alaa50.aa@gmail.com>
2021-02-05 01:26:18 +0200

    Adding layouts & activity classes

---
+++ app/src/main/java/com/example/uber/UserLoginActivity.java
@@ -0,0 +1,52 @@
+package com.example.uber;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserLoginActivity extends AppCompatActivity {
+
+
+    private EditText mEmail, mPassword;
+    private Button mLogin, mRegistration;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_login);
+
+
+        mEmail = (EditText) findViewById(R.id.email_input);
+        mPassword = (EditText) findViewById(R.id.password_input);
+
+        mLogin = (Button) findViewById(R.id.login_button);
+        mRegistration = (Button) findViewById(R.id.registration_button);
+
+
+        mRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+}
+
